009a909fb234818b6660716bc3253cdc2a5bf46e
putri macrcella <ptrcella236@gmail.com>
2023-11-18 16:05:12 +0700

    pembaruan

---
+++ CeMeals.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+        showSlides(slideIndex);
+  
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+  
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("slide");
+          if (n > slides.length)
+          {
+            slideIndex = 1;
+          }
+          if (n < 1)
+          {
+            slideIndex = slides.length
+          }
+          for (i = 0; i < slides.length; i++)
+          {
+            slides[i].style.display = "none";
+          }
+          slides[slideIndex-1].style.display = "block";
+        }
+
+        VanillaTilt.init(document.querySelectorAll(".box"), {
+            max: 25,
+            speed: 400,
+        })
+
+        $(document).ready(function(){
+            $('.container').container({
+                padding: 200
+            });
+            autoplay();
+            function autoplay(){
+                $('.container').container('next');
+                setTimeout(autoplay, 4500);
+            }
+        });
+
